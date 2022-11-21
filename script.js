58135072093e6ae1b872c9192a00dda5caa6bed3
Vineeth Serigar <vineeth.serigar@netcracker.com>
2022-11-21 14:25:53 +0530

    bug fixing

--- script.js
+++ script.js
@@ -7,19 +7,10 @@ const startTimer = () => {
     let hour = document.getElementById("hour");
     let minute = document.getElementById("minute");
     let second = document.getElementById("second");
-    // let miliSecond = document.getElementById("miliSecond");
     let h = Number.parseInt(hour.innerHTML);
     let m = Number.parseInt(minute.innerHTML);
     let s = Number.parseInt(second.innerHTML);
-    // let ms = Number.parseInt(miliSecond.innerHTML);
     let timer = setInterval(() => {
-      //   setInterval(() => {
-      //     ms++;
-      //     if (ms == 20) {
-      //       ms = 0;
-      //     }
-      //     miliSecond.innerHTML = ms < 10 ? "0" + ms : ms;
-      //   }, 10);
       s++;
       if (s == 60) {
         m++;
@@ -33,7 +24,7 @@ const startTimer = () => {
         h = 0;
       }
       hour.innerHTML = h < 10 ? "0" + h : h;
-      minute.innerHTML = m < 10 ? "0" + m : s;
+      minute.innerHTML = m < 10 ? "0" + m : m;
       second.innerHTML = s < 10 ? "0" + s : s;
     }, 1000);
     t = timer;
@@ -48,7 +39,5 @@ const pauseTimer = () => {
 const resetTimer = () => {
   clearInterval(t);
   timerRunning = false;
-  hour.innerHTML = "00";
-  minute.innerHTML = "00";
-  second.innerHTML = "00";
+  hour.innerHTML = minute.innerHTML = second.innerHTML = "00";
 };
